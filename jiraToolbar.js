877df5c667677a0ed35a1aee7c1cb6b5274fc486
takeuchi <g.takeuchi@gmail.com>
2012-04-23 20:07:19 -0300

    Lazy integration with jquery

--- jiraToolbar.js
+++ jiraToolbar.js
@@ -1,11 +1,9 @@
-alert('addin');
-if ($('#taksan_jiraToolbar') == null) {
-	var jiraToolbarSingleton = $('<div id="taksan_jiraToolbar" style="position:fixed;top:5px;left:5px; width: 100%">#####</div>');
-	$('body').prepend(jiraToolbarSingleton)
-
-	function addToToolbar(obj)
-	{
-		alert(obj)
-		jiraToolbarSingleton.append(obj);
+function addToToolbar(obj)
+{
+	if ($('#taksan_jiraToolbar') == null) {
+		var jiraToolbarSingleton = $('<div id="taksan_jiraToolbar" style="position:fixed;top:5px;left:5px; width: 100%">#####</div>');
+		$('body').prepend(jiraToolbarSingleton)
 	}
+
+	jiraToolbarSingleton.append(obj);
 }
