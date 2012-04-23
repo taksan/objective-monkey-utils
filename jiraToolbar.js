78e55a8eba87756cee9630078b8ceeb973de591a
takeuchi <g.takeuchi@gmail.com>
2012-04-23 20:10:04 -0300

    Fixing bug

--- jiraToolbar.js
+++ jiraToolbar.js
@@ -1,7 +1,8 @@
 function addToToolbar(obj)
 {
+	var jiraToolbarSingleton;
 	if ($('#taksan_jiraToolbar') == null) {
-		var jiraToolbarSingleton = $('<div id="taksan_jiraToolbar" style="position:fixed;top:5px;left:5px; width: 100%">#####</div>');
+		jiraToolbarSingleton = $('<div id="taksan_jiraToolbar" style="position:fixed;top:5px;left:5px; width: 100%">#####</div>');
 		$('body').prepend(jiraToolbarSingleton)
 	}
 
