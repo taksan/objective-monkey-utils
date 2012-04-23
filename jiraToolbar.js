d4be2adeac969d4322d79bd304622900f478001f
gtakeuchi <gabriel.takeuchi@objective.com.br>
2012-04-23 13:39:28 -0300

    Renaming and changing the script. It will not be a grease monkey script anymore

---
+++ jiraToolbar.js
@@ -0,0 +1,10 @@
+if ($('#taksan_jiraToolbar') == null) {
+	var jiraToolbarSingleton = $('<div id="taksan_jiraToolbar" style="position:fixed;top:5px;left:5px; width: 100%">#####</div>');
+	$('body').prepend(jiraToolbarSingleton)
+
+	function addToToolbar(obj)
+	{
+		alert(obj)
+		jiraToolbarSingleton.append(obj);
+	}
+}
