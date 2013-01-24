235063e8da50f6267671245e9bfd4d9f7672ea2f
takeuchi <g.takeuchi@gmail.com>
2013-01-24 09:02:00 -0200

    Fixed annoying bug that would prevent arrow keys from working on input fields

--- keyboarNavigation.js
+++ keyboarNavigation.js
@@ -1,6 +1,10 @@
 function addKeyboardNavigation(PREV,NEXT)
 {
 	$(document).keydown(function(e){
+		var ignore=["INPUT","SELECT","TEXTAREA"];
+		var typeOfFocusedComponent=document.activeElement.tagName.toUpperCase().trim();
+		if ($.inArray(typeOfFocusedComponent, ignore)>=0)
+			return true;
 		if (e.keyCode == 37) { 
 			if (PREV.size()==0)
 				return false;
